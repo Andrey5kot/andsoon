73407d9611e0c0def4ae0d0691874da1437504f4
Odi-cs <fry.pichenka@gmail.com>
2021-11-08 15:46:17 +0200

    sign up layout

--- app/src/main/java/com/kot/andsoon/MainActivity.kt
+++ app/src/main/java/com/kot/andsoon/MainActivity.kt
@@ -2,10 +2,16 @@ package com.kot.andsoon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        val sign_up_textbutton: Button =findViewById(R.id.sign_up_textbutton)
+        sign_up_textbutton.setOnClickListener{
+            setContentView(R.layout.activity_sign_up)
+        }
+
     }
 }
